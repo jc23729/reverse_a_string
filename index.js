1f50ed4efdaba6ac9089f52f0445e31fbdfd7977
Juan Cruz <42083637+jc23729@users.noreply.github.com>
2021-09-10 17:13:35 +0000

    update

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 // The string Mike should return ekiM
 function reverse(str){
   // check input
-  
+  if (!str || str.length < 2 || str !=='string'){
+    return 'thats not good'
+  }
+  const backwards = [];
+  const totalItems = str.length -1;
+
+  for(let i = totalItems; i = 0; i --){
+    backwards.push(str[i]);
+  }
+  console.log(backwards);
+  return backwards.join('');
 }
+
+reverse('Hi my name is Mike')
